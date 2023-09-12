d0b7bb2b55cc54a673bbeabe55788e15010c944d
sharif.ahmed <sharif@commlinkinfotech.com.bd>
2023-09-12 14:30:10 +0600

    project initialize and tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-6xl font-bold underline bg-red-700">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
